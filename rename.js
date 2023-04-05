aa9115d3445596da85ef9ddab20d3ef4fb91a6a3
SASHiBHUMARAJU2 <sasi.654forever@gmail.com>
2023-04-05 15:13:51 +0530

    first commit

--- rename.js
+++ rename.js
@@ -2,21 +2,13 @@ const fs = require("fs")
 const currPath = "./build"
 const newPath = "./docs"
 
-try{
+
   fs.rm(newPath, { recursive: true }, (err) => {
     if (err) {
       console.error("no files found for name docs");
       return;
     }
     console.log(`${newPath} is deleted!`);
-  });
-}
-
-catch( exc ) {
-  console.log(exc);
-}
-   
-    
     fs.rename(currPath, newPath, function(err) {
       if (err) {
         console.log("no files found for name build")
@@ -24,4 +16,11 @@ catch( exc ) {
         console.log("Successfully renamed the directory.")
       }
     })
+    
+  });
+
+
+
+  
+
     
