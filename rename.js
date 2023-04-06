1d6d2113bce1d9554c5df044e0293e8f8aa8359a
SASHiBHUMARAJU2 <sasi.654forever@gmail.com>
2023-04-06 23:04:36 +0530

    updated

--- rename.js
+++ rename.js
@@ -2,21 +2,21 @@ const fs = require("fs")
 const currPath = "./build"
 const newPath = "./docs"
 
-
   fs.rm(newPath, { recursive: true }, (err) => {
     if (err) {
       console.error("no files found for name docs");
       return;
     }
     console.log(`${newPath} is deleted!`);
-    fs.rename(currPath, newPath, function(err) {
-      if (err) {
-        console.log("no files found for name build")
-      } else {
-        console.log("Successfully renamed the directory.")
-      }
-    })
-    
+
+        fs.rename(currPath, newPath, function(err) {
+          if (err) {
+            console.log("no files found for name build")
+          } else {
+            console.log("Successfully renamed the directory.")
+          }
+        })
+        
   });
 
 
--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
-
 import './App.css';
 
 function App() {
   
   return (
-   <h1>Pixxie hi</h1>
+   <h1 >Pixxie hi</h1>
   );
 }
 
