3b4ae7067ab2e96802798d57274ca8d30e7002a6
SASHiBHUMARAJU2 <sasi.654forever@gmail.com>
2023-04-07 13:23:12 +0530

    'updated'

--- src/hooks/ChangeTheme.js
+++ src/hooks/ChangeTheme.js
@@ -1,18 +1,24 @@
 import {useEffect,useState} from 'react';
 
 // change theme hook
-function useChangeTheme (intialTheme) {
+function useChangeTheme () {
 
     //  intial theme
+    const currenttheme = localStorage.getItem("theme")
+    const intialTheme = ( currenttheme == null )? "light-theme" : currenttheme
     const [theme,setTheme] = useState(intialTheme);
 
     // change current theme state
     const changeThemeColor = () => {
         if (theme === 'light-theme') {
+          localStorage.setItem("theme","dark-theme")
           setTheme('dark-theme');
           return;
         }
+
+        localStorage.setItem("theme","light-theme")
         setTheme('light-theme');
+       
       }
     
     //   change app theme color
