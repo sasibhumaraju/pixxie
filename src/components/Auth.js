8880c240f9acca7423f75e25fac4f96878d30786
SASHiBHUMARAJU2 <sasi.654forever@gmail.com>
2023-04-13 20:28:00 +0530

    updated

--- src/components/Auth.js
+++ src/components/Auth.js
@@ -1,12 +1,16 @@
 import React from "react";
 import Login from "./Login";
-import LogoDark from '../assets/logo-vector-dark.png'
+import LogoDark from '../assets/logo-vector-dark.png';
+import MobLogoDark from '../assets/logo-text-dark.png'
 
 function Auth () {
     return (
         <div className="auth"> 
             <div className="logo">
                 <img src={LogoDark}></img>
+            </div>
+            <div className="mob-logo">
+                <img src={MobLogoDark}></img>
             </div>
             <Login></Login>
          </div>
