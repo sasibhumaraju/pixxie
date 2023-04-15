db9a0cfd1fac14aacee91deb93e809e40ecc717d
SASHiBHUMARAJU2 <sasi.654forever@gmail.com>
2023-04-15 21:58:46 +0530

    uptaed

--- src/components/Login.js
+++ src/components/Login.js
@@ -29,18 +29,7 @@ function Login (props) {
             <div className='forgot-passowrd-button'> Forgot Password? </div>
             <div className='new-account'> Don't have an account? <span onClick={props.handleChange} style={{color:"#2a85ff", cursor:"pointer"}}>Sign up</span>  </div>
             </div>
-            <div className='login'>
-            <div className='login-card'>
-            <div className='heading'>  Login </div>
-            <form onSubmit={signIn} >
-                <input type='text'  placeholder='email or username' value={email} onChange={setEmail}></input> 
-                <input type='password' placeholder='password' value={password} onChange={setPassword}></input>
-                <button type='submit' className='submit' >Login</button>
-            </form>
-            <div className='forgot-passowrd-button'> Forgot Password? </div>
-            <div className='new-account'> Don't have an account? <span onClick={props.handleChange} style={{color:"#2a85ff", cursor:"pointer"}}>Sign up</span>  </div>
-            </div>
-        </div>
+            
         </div>
 
     );
